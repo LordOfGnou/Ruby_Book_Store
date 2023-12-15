dc454f52ffce2ced6c2c5cf5960f6d3b7b63d7ab
LordOfGnou <83947403+LordOfGnou@users.noreply.github.com>
2023-12-15 16:43:10 +0100

    adding get book by id

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
    def get_all_books
-      render json: { "data": "all books" }
+      @books = Book.all
+      render json: { "data": @books }
+   end
+
+   def get_book_by_id
+      @book = Book.find(params[:id])
+      render json: { "data": @book }
    end
 end
