a545954dd9dbe31650a27dfb345eddf0d476a564
LordOfGnou <83947403+LordOfGnou@users.noreply.github.com>
2023-12-15 16:26:50 +0100

    adding a few books

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Book.destroy_all
+Book.reset_pk_sequence
+
+Book.create(
+   [
+      {
+         title: 'Martine decouvre Jean',
+         year: 1995
+      },
+      {
+         title: 'Jean a la piscine avec Tonton',
+         year: 2007
+      },
+      {
+         title: 'Le petit Nicolas',
+         year: 1959
+      },
+      {
+         title: 'La Horde du Contre-Vent',
+         year: 2004
+      },
+      {
+         title: "Bart ou chroniques d'un cretin trouillard qui devait sauver le monde",
+         year: 2010
+      },
+   ]
+)
